6599f46cec252bd66925dbdcf7727afc8d327127
Takuty-a11y <takuty.008.awenite.1121@gmail.com>
2022-06-22 23:26:59 +0900

    Route&Page Create

---
+++ src/router/Router.tsx
@@ -0,0 +1,25 @@
+import { FC, memo } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Login } from "../components/pages/Login";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { homeRoutes } from "./HomeRoutes";
+
+export const Router: FC = memo(() => {
+  return (
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path="home">
+        {homeRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<HeaderLayout>{route.children}</HeaderLayout>}
+          />
+        ))}
+      </Route>
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+});
